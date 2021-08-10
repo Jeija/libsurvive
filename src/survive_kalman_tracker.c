b43cba3e5d79c2c28cab2bb78ebb3b9d0e344d58
Jeija <jeija@jeija.net>
2021-08-10 18:05:48 +0200

    Use observation_time instead of light_time

--- src/survive_kalman_tracker.c
+++ src/survive_kalman_tracker.c
@@ -409,7 +409,7 @@ void survive_kalman_tracker_integrate_imu(SurviveKalmanTracker *tracker, PoserDa
 
 
 	FLT zvu_var = isStationary ? tracker->zvu_stationary_var : tracker->zvu_moving_var;
-	if (time - tracker->last_light_time > .1) {
+	if (time - tracker->last_observation_time > .1) {
 		zvu_var = tracker->zvu_no_light_var;
 	}
 	if (zvu_var >= 0) {//time - tracker->last_light_time > .1) {//|| isStationary || fabs(1 - norm) < .001 ) {
